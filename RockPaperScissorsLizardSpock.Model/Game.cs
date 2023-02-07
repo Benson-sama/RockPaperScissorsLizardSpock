9b7d7669f45ea0888c2575f70f3ad21d98ca19f7
Benjamin BOGNER <BOGNER.Benjamin@outlook.at>
2023-02-07 14:39:52 +0100

    Added Game class.

---
+++ RockPaperScissorsLizardSpock.Model/Game.cs
@@ -0,0 +1,58 @@
+﻿using RockPaperScissorsLizardSpock.Model.Moves;
+
+namespace RockPaperScissorsLizardSpock.Model;
+
+public class Game
+{
+    private Move? _firstPlayerMove;
+    private bool _isFirstPlayerMoveFinal;
+    private Move? _secondPlayerMove;
+    private bool _isSecondPlayerMoveFinal;
+
+    public Game(string firstPlayerName, string secondPlayerName)
+    {
+        if (string.IsNullOrWhiteSpace(firstPlayerName))
+            throw new ArgumentNullException(firstPlayerName);
+
+        if (string.IsNullOrWhiteSpace(secondPlayerName))
+            throw new ArgumentNullException(secondPlayerName);
+
+        FirstPlayerName = firstPlayerName;
+        SecondPlayerName = secondPlayerName;
+    }
+
+    public string FirstPlayerName { get; }
+
+    public Move? FirstPlayerMove
+    {
+        get => _firstPlayerMove;
+        set
+        {
+            if (_isFirstPlayerMoveFinal)
+                throw new InvalidOperationException();
+
+            _firstPlayerMove = value;
+            _isFirstPlayerMoveFinal = true;
+        }
+    }
+
+    public string SecondPlayerName { get; }
+
+    public Move? SecondPlayerMove
+    {
+        get => _secondPlayerMove;
+        set
+        {
+            if (_isSecondPlayerMoveFinal)
+                throw new InvalidOperationException();
+
+            _secondPlayerMove = value;
+            _isSecondPlayerMoveFinal = true;
+        }
+    }
+
+    public bool BothMovesSelected
+    {
+        get => FirstPlayerMove is not null && SecondPlayerMove is not null;
+    }
+}
