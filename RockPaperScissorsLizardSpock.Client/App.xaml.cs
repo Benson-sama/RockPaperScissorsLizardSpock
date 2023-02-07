b2a17237ec85fd4cbacbdc1beda31797eb5129bb
Benjamin BOGNER <BOGNER.Benjamin@outlook.at>
2023-02-07 11:27:13 +0100

    Set app size and position for windows debug sessions.

--- RockPaperScissorsLizardSpock.Client/App.xaml.cs
+++ RockPaperScissorsLizardSpock.Client/App.xaml.cs
@@ -8,4 +8,18 @@ public partial class App : Application
 
         MainPage = new AppShell();
     }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+#if DEBUG && WINDOWS
+        window.Width = 360;
+        window.Height = 780;
+        window.X = (2560 / 2) - (window.Width / 2);
+        window.Y = (1440 / 2) - (window.Height / 2);
+#endif
+
+        return window;
+    }
 }
